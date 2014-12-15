c457e885050e758bdb9932aeb18fbb1f04bb92a2
James Nathaniel Hanna <nhanna@integracore.com>
2014-12-15 21:15:09 +0000

    removed application controller to avoid a file load error

--- lib/mass_record/version.rb
+++ lib/mass_record/version.rb
@@ -1,3 +1,3 @@
 module MassRecord
-  VERSION = "0.0.4.1" # fixed bug in logging errors when no errors were present
+  VERSION = "0.0.4.2" # removed application controller to avoid a file load error
 end
