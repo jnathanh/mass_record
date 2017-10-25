aae0554cd0005be8e80bc99373182cb5044271db
Nathan Hanna <jnathanh@gmail.com>
2017-10-25 11:36:35 -0600

    updated version to 0.0.4.9

--- lib/mass_record/version.rb
+++ lib/mass_record/version.rb
@@ -1,3 +1,4 @@
 module MassRecord
-  VERSION = "0.0.4.8" # added a temporary fix for sqlserver bit type conversion from false
+  VERSION = "0.0.4.9" # implemented a custom Array#pretty_inspect method, as well as error handling in case it doesn't work, and fixed gem dependancy versions for integracore update
+  # VERSION = "0.0.4.8" # added a temporary fix for sqlserver bit type conversion from false
 end
